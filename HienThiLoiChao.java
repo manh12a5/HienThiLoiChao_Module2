0449d83e0de6b34310e71765130f22f54b677429
manh12a5 <manh12a5@gmail.com>
2020-12-09 17:22:13 +0700

    'Module2'

---
+++ HienThiLoiChao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap ten cua ban: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
